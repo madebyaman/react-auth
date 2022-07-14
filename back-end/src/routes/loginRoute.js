f36ebe685883bf5fbca48ddc4dcb769e843254ce
Aman Thakur <builtbyaman@gmail.com>
2022-07-14 16:20:00 +0530

    added login

--- back-end/src/routes/loginRoute.js
+++ back-end/src/routes/loginRoute.js
@@ -5,5 +5,23 @@ import { getDbConnection } from '../db';
 export const loginRoute = {
   path: '/api/login',
   method: 'post',
-  handler: async (req, res) => {},
+  handler: async (req, res) => {
+    const { email, password } = req.body;
+    const db = getDbConnection('react-auth-db');
+    const user = await db.collection('users').findOne({ email });
+    if (!user) return res.sendStatus(401);
+
+    const { _id: id, isVerified, passwordHash, info } = user;
+    const isCorrect = await bcrypt.compare(password, passwordHash);
+    if (!isCorrect) return res.sendStatus(401);
+    jwt.sign(
+      { id, isVerified, email, info },
+      process.env.JWT_SECRET,
+      { expiresIn: '2d' },
+      (err, token) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json({ token });
+      }
+    );
+  },
 };
